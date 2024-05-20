d2177aa9789f469c6fb15f3aef751abac09e0511
Eric Curtin <ecurtin@redhat.com>
2024-05-20 16:33:43 +0100

    Read various kargs
    The following kargs could be used:
    
      mount-sysroot.root=, root=, mount-sysroot.rootfstype=, rootfstype=
    
    Co-developed-by: Alexander Larsson <alexl@redhat.com>
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+clang-format --style=chromium -i mount-sysroot.c
 gcc -O3 -Wall -Wextra -pedantic -fanalyzer -o mount-sysroot.gcc mount-sysroot.c -lblkid &
 clang -O3 -Wall -Wextra -pedantic -o mount-sysroot.clang mount-sysroot.c -lblkid &
 wait
--- mount-sysroot.c
+++ mount-sysroot.c
@@ -1,36 +1,91 @@
 #include <blkid/blkid.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mount.h>
 #include <unistd.h>
 
-static int usage(void) {
-  fprintf(stderr, "Usage: mount-sysroot -t <fstype> <device> <mountpoint>\n");
-  return EXIT_FAILURE;
+#define autofree __attribute__((cleanup(cleanup_free)))
+#define autofclose __attribute__((cleanup(cleanup_fclose)))
+
+static inline void cleanup_free(void* p) {
+  free(*(void**)p);
 }
 
-int main(const int argc, const char* const argv[]) {
-  if (argc < 4)
-    return usage();
+static inline void cleanup_fclose(FILE** stream) {
+  if (*stream)
+    fclose(*stream);
+}
 
-  // Parse command-line arguments
-  const char* fstype = NULL;
-  const char* device = NULL;
-  const char* mountpoint = NULL;
-  for (int i = 1; i < argc; ++i) {
-    if (strcmp(argv[i], "-t") == 0 && i + 1 < argc) {
-      fstype = argv[++i];
-    } else if (device == NULL) {
-      device = argv[i];
-    } else if (mountpoint == NULL) {
-      mountpoint = argv[i];
-    }
+static char* read_proc_cmdline(void) {
+  autofclose FILE* f = fopen("/proc/cmdline", "r");
+  if (!f) {
+    perror("Failed to open /proc/cmdline");
+    return NULL;
   }
 
-  if (fstype == NULL || device == NULL || mountpoint == NULL)
-    return usage();
+  char* cmdline = NULL;
+  size_t len;
+
+  /* Note that /proc/cmdline will not end in a newline, so getline
+   * will fail unelss we provide a length.
+   */
+  if (getline(&cmdline, &len, f) < 0) {
+    perror("Failed to read /proc/cmdline");
+    return NULL;
+  }
+
+  /* ... but the length will be the size of the malloc buffer, not
+   * strlen().  Fix that.
+   */
+  len = strlen(cmdline);
+  if (cmdline[len - 1] == '\n')
+    cmdline[len - 1] = '\0';
+
+  return cmdline;
+}
+
+static char* find_proc_cmdline_key(const char* cmdline, const char* key) {
+  const size_t key_len = strlen(key);
+  for (const char* iter = cmdline; iter;) {
+    const char* next = strchr(iter, ' ');
+    if (strncmp(iter, key, key_len) == 0 && iter[key_len] == '=') {
+      const char* start = iter + key_len + 1;
+      if (next)
+        return strndup(start, next - start);
+
+      return strdup(start);
+    }
+
+    if (next)
+      next += strspn(next, " ");
+
+    iter = next;
+  }
+
+  return NULL;
+}
+
+int main(void) {
+  const autofree char* cmdline = read_proc_cmdline();
+  if (cmdline == NULL)
+    return EXIT_FAILURE;
+
+  autofree char* device = find_proc_cmdline_key(cmdline, "mount-sysroot.root");
+  if (device == NULL)
+    device = find_proc_cmdline_key(cmdline, "root");
+
+  if (device == NULL) {
+    perror("No mount-sysroot.root= or root= karg present in /proc/cmdline");
+    return EXIT_FAILURE;
+  }
+
+  autofree char* fstype =
+      find_proc_cmdline_key(cmdline, "mount-sysroot.rootfstype");
+  if (fstype == NULL)
+    fstype = find_proc_cmdline_key(fstype, "rootfstype");
 
   if (strncmp(device, "PARTUUID=", 9) == 0 ||
       strncmp(device, "PARTLABEL=", 10) == 0) {
@@ -41,7 +96,7 @@ int main(const int argc, const char* const argv[]) {
     }
   }
 
-  if (mount(device, mountpoint, fstype, 0, NULL) == -1) {
+  if (mount(device, "/sysroot", fstype, 0, NULL) == -1) {
     perror("Error mounting device");
     return EXIT_FAILURE;
   }
